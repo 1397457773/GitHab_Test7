7848e6987c8f16e7d0e17a2710daffb70b009b31
1397457773 <1397457773@qq.com>
2017-08-14 23:02:45 +0800

    第一次提交 完成框架的搭建
    时间：2017年8月14日23:02:27
    框架师：李俊岐

---
+++ app/src/main/java/com/jiyun/githab_test2/Demo.java
@@ -0,0 +1,15 @@
+package com.jiyun.githab_test2;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Lenovo on 2017/8/14.
+ */
+
+public class Demo {
+    private void set(){
+        Log.e("TAG", "set01: " );
+    }
+}
